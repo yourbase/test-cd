b9b30b3e07894df06460081b6ca0b3738b284fe6
Yves Junqueira <yves.junqueira@gmail.com>
2018-02-08 14:01:22 -0800

    Minor change

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type helloWorldHandler struct{}
 
 func (h helloWorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello, World")
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
