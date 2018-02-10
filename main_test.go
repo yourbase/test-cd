dd12cb0670dbf75cdc878d23c1908eb03946f56c
Yves Junqueira <yves.junqueira@gmail.com>
2018-02-09 19:01:12 -0800

    should pass CI/CD

--- main_test.go
+++ main_test.go
@@ -28,7 +28,7 @@ func TestHelloWorldHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `Hello, World!`
+	expected := `Hello, World`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
